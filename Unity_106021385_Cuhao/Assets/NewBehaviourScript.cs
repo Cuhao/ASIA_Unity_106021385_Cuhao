b9f35781cfaa35dd6c77ab4e6037202b4ff6d9a7
Cuhao <32056058+Cuhao@users.noreply.github.com>
2020-01-08 11:22:38 +0800

    106021385

---
+++ Unity_106021385_Cuhao/Assets/NewBehaviourScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [Header("寶箱數量介面")]
+    public Text textCount;
+    [Header("提示畫面")]
+    public GameObject objTop;
+    public int propCount;
+    public int propTotal = 10;
+    private void OnCollisionEnter(Collision collision)
+    {
+        print(collision.gameObject);
+        if(collision.gameObject.tag == "寶箱")
+        {
+            Destroy(collision.gameObject);
+            propCount ++;
+            textCount.text = "寶箱" + propCount + "/ 10";
+        }
+    }
+    private void Update()
+    {
+        GameOver();
+    }
+    private void GameOver()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            //Application.LoadLevel("遊戲場景");舊版API
+            SceneManager.LoadScene("遊戲場景");
+        }
+    }
+}
